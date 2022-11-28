1bc7cbba3b6282b6714898c7f9dd88f83862acb8
Joshua Reno <jralphur@gmail.com>
2022-11-27 19:09:26 -0800

    output

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strings"
 
+	jack_compiler "github.com/renojcpp/n2t-compiler/compiler"
 	jack_tokenizer "github.com/renojcpp/n2t-compiler/tokenizer"
 )
 
@@ -14,28 +15,29 @@ func main() {
 	for _, arg := range args {
 		dirs, err := os.ReadDir(arg)
 		if err != nil {
-			fmt.Errorf("no such directory: %s", arg)
+			fmt.Printf("no such directory: %s", arg)
 		}
 
 		for _, entry := range dirs {
 			if strings.HasSuffix(entry.Name(), ".jack") {
 				file, err := os.Open(fmt.Sprintf("%s/%s", arg, entry.Name()))
 				if err != nil {
-					fmt.Errorf("failed to open file: %s", arg)
+					fmt.Printf("failed to open file: %s", arg)
 				}
 
 				tokens, err := jack_tokenizer.Tokenize(file)
 
 				if err != nil {
-					fmt.Errorf("failed to tokenize: %s", err)
+					fmt.Printf("failed to tokenize: %s", err)
 				}
 
 				toOut := jack_compiler.ParseGrammar(tokens)
 				fmt.Println("outputting")
-				err = toOut(os.Stdout)
+				f, _ := os.Create(fmt.Sprintf("%s/%s", arg, entry.Name()+".vm"))
+				err = toOut(f)
 
 				if err != nil {
-					fmt.Errorf("%s", err.Error())
+					fmt.Printf("%s", err.Error())
 				}
 			}
 		}
